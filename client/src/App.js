daae35923e8bacc1b4550c5d15fa1bc8ad7fb9bb
MAGNUSSON Pontus <pontus.magnusson@protonmail.com>
2019-08-28 16:14:13 +0200

    Add k8s configurations

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Fib calculator</h1>
+          <h1>Fib calculator in kubernetes!</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other page</Link>
         </header>
